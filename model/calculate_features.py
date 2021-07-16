gutenberch richter b and a calculation is fixed, now it uses log, for a logaritmic loss

model/calculate_features.py
@@ -81,16 +81,16 @@ class CalculateFeatures:
 
         features_last_t_array = np.array(self.features.lastT, dtype='datetime64[s]')
         # F3_COLUMNS
-        self.features[model.F3_COLUMNS] = self._dt_max_magnitude(features_last_t_array,
+        self.features[model.F3_COLUMNS[0]] = self._dt_max_magnitude(features_last_t_array,
                                                                  -self.daysBackward, all_dates,
                                                                  all_magnitudes)
         # F4_COLUMNS
-        self.features[model.F4_COLUMNS] = self._dt_max_magnitude(features_last_t_array,
+        self.features[model.F4_COLUMNS[0]] = self._dt_max_magnitude(features_last_t_array,
                                                                  self.daysForward,
                                                                  all_dates,
                                                                  all_magnitudes)
         # F5_COLUMNS
-        self.features[model.F5_COLUMNS] = self.features[model.F4_COLUMNS] >= mag_threshold
+        self.features[model.F5_COLUMNS[0]] = self.features[model.F4_COLUMNS] >= mag_threshold
 
         # TRIM TIME AND Nan's
         if trim_features:
@@ -189,11 +189,14 @@ class CalculateFeatures:
 
     @staticmethod
     def gutenberg_richter_law(m, a, b):
-        with np.errstate(over='ignore'):
-            return np.power(10, a - b * m, dtype=longdouble)
+            return np.power(10, a - np.multiply(b, m, dtype=longdouble), dtype=longdouble)
 
     @staticmethod
-    def _cumcount_sorted_unique(array: Iterable, n: int = None):
+    def log_gutenberg_richter_law(m, a, b):
+            return a - np.multiply(b, m, dtype=longdouble)
+
+    @staticmethod
+    def _cumcount_sorted_unique(array: Iterable):
         unique, counts = np.unique(array, return_counts=True)
         view = np.flip(counts, 0)
         np.cumsum(view, 0, out=view)
@@ -201,19 +204,21 @@ class CalculateFeatures:
 
     @staticmethod
     def _gutenberg_richter_curve_fit(unique, counts):
-        return curve_fit(CalculateFeatures.gutenberg_richter_law, unique, counts)[0]
+        log_counts = np.log10(counts.astype(longdouble), dtype=longdouble)
+        return curve_fit(CalculateFeatures.log_gutenberg_richter_law, unique, log_counts)[0]
 
     @staticmethod
-    def gutenberg_richter_curve_fit(magnitude, n: int = None):
+    def gutenberg_richter_curve_fit(magnitude):
         """
         Use non-linear least squares to fit a gutenberg_richter_curve's values a and b, to data.
         :param magnitude:
         :param n:
         :return:
         """
-        unique, counts = CalculateFeatures._cumcount_sorted_unique(magnitude, n)
 
-        return curve_fit(CalculateFeatures.gutenberg_richter_law, unique, counts)[0]
+        unique, counts = CalculateFeatures._cumcount_sorted_unique(magnitude)
+        log_counts = np.log10(counts.astype(longdouble), dtype=longdouble)
+        return curve_fit(CalculateFeatures.log_gutenberg_richter_law, unique, log_counts)[0]
 
     #
     # T - Time elapsed for last “n” seismic events

model/utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.patches import ConnectionPatch
+import pandas as pd
+import model
+
 
 def b_slope_analysis(df, cut):
     fig, axes = plt.subplots(nrows=1, ncols=2)
@@ -8,8 +11,10 @@ def b_slope_analysis(df, cut):
     df = df[["Magnitude"]].groupby(["Magnitude"]).size().reset_index(name='counts')
     df_cut = df[df.Magnitude >= cut]
     fig.set_size_inches(12, 4)
-    df.plot(ax=axes[0], title="full", x="Magnitude", y="counts")
+
+    df.plot(ax=axes[0], title="full", x="Magnitude", y="counts").axvline(cut, c="red", label="cutoff")
     df_cut.plot(ax=axes[1], title="cut", x="Magnitude", y="counts")
+
     plt.show()
 
 
@@ -23,14 +28,15 @@ def minimum_year_analysis(df, years, ylim=None):
     df_yearly_magnitude_mean = df_yearly_magnitude.mean()
     df_yearly_count = df_yearly_magnitude.count()
     df_yearly_variance = df_yearly_magnitude.var()
-    df_yearly_b_val = df_yearly_magnitude_mean.apply(lambda x: (np.log(np.e)) / (x - 3.8))
+    df_yearly_curve_val_b = df_yearly_magnitude.apply(
+        lambda x: model.CalculateFeatures.gutenberg_richter_curve_fit(x)[1] if x.shape[0] > 2 else 0)
     df_yearly_min = df_yearly_magnitude.min()
     fig, axes = plt.subplots(nrows=5, ncols=1)
     fig.suptitle('Minimum Year Analysis')
     fig.set_size_inches(12, 8)
     df_yearly_magnitude_mean.plot(ax=axes[0], kind="line", xlabel="", title="Magnitude Mean", legend=[])
     df_yearly_count.plot(ax=axes[1], kind="line", title="Count", xlabel="", xticks=[])
-    df_yearly_b_val.plot(ax=axes[2], kind="line", title="b-value", xlabel="", xticks=[])
+    df_yearly_curve_val_b.plot(ax=axes[2], kind="line", title="b-value", xlabel="2", xticks=[])
     df_yearly_min.plot(ax=axes[3], kind="line", title="MinMag", xlabel="", xticks=[])
     df_yearly_variance.plot(ax=axes[4], kind="line", title="Variance")
     axes[0].xaxis.set_ticks_position('top')
@@ -43,4 +49,3 @@ def minimum_year_analysis(df, years, ylim=None):
         fig.add_artist(ConnectionPatch(xyA=[year, top], xyB=[year, bottom], coordsA="data", coordsB="data",
                                        axesA=axes[0], axesB=axes[4], color="green"))
     plt.show()
-